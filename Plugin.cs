6bc12703d8b38f0743a5a675b93b6138b3929d07
Unknown Kebonaut <78289857+Kerbonaut1@users.noreply.github.com>
2024-10-15 23:53:20 +1100

    Added New BepinEx Version
    It Doesn't work rn

---
+++ Plugin.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+
+namespace ModTemplate
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        // Until I make this a nuget package please replace the path in Directory.Build.props with your own game path
+        // Also go to PluginInfo.cs and put your mods info in it
+        void Awake()
+        {
+            // This is called once when the game is started
+            Logger.LogInfo("Get ready mother clucker!");
+        }
+
+        public float MoveSpeed = 2.5f;
+
+
+        // when a new scene is loaded
+        void OnLevelWasLoaded(int level)
+        {
+            // This is called when a new scene is loaded
+            Time.timeScale = MoveSpeed;
+
+            // set fixed time step to 0.02
+            Time.fixedDeltaTime = 0.02f;
+            MoveSpeed = 2.5f;
+        }
+
+        // On fixed update is called every frame
+        void FixedUpdate()
+        {
+            if (Time.timeScale > 2.0f)
+            {
+                MoveSpeed += 0.01f * Time.deltaTime;
+            }
+        }
+
+        // On update is called every frame
+        void Update()
+        {
+            // This is called every frame
+            if (Time.timeScale > 2.0f)
+            {
+                // set time scale to 10
+                Time.timeScale = MoveSpeed;
+
+                // set fixed time step to 0.02
+                Time.fixedDeltaTime = 0.02f;
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ModTemplate
+{
+    internal class PluginInfo
+    {
+        // Replace ModTemplate with your mods name
+        public const string GUID = "com.sbbcmods.ModTemplate";
+        // Replace this with your mods name!
+        public const string Name = "BawkExtremeBepinEx";
+        public const string Version = "0.1.0";
+    }
+}
